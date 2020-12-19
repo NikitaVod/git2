f1b7222530915c621e5a7015fd0f4229f78cbe59
NikitaVod <2001dk9@mail.ru>
2020-12-19 17:33:21 +0300

    code completed

--- src/Git2.java
+++ src/Git2.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Git2 {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello, my name is " + name);
     }
 }
